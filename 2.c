80c1b32f794bfc0e9ba5af90268e4e81206941ba
Artur Yakupov <art.kazan@mail.ru>
2023-12-14 10:39:56 +0300

    6_lab

--- 2.c
+++ 2.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
-int main() {
-	printf("Hello, Ubuntu!\n");
+void exit_proccess(int signal) {
+	exit(0);
+}	
+
+void infinite_reporting_loop(pid_t ppid, pid_t pid) {
+	struct timeval time;
+	long int hours, minutes, seconds, miliseconds;
+	while(1) {
+		gettimeofday(&time, NULL);
+		hours = time.tv_sec/3600%24;
+		minutes = time.tv_sec/60%60;
+		seconds = time.tv_sec%60;
+		miliseconds = time.tv_usec/1000;
+		printf("This is PId=%d (parent is %d)\nReport time %ld:%ld:%ld:%ld\n\n",pid, ppid, hours, minutes, seconds, miliseconds);
+	       sleep(1);	
+	}
+}
+
+int main(){
+	signal(SIGINT, exit_proccess);
+
+	pid_t pid1;
+	pid_t pid2;
+	pid1 = fork();
+	if (pid1 == -1) {
+		printf("Error while creating first fork.\n");
+		exit(1);
+	}
+	else if (pid1 == 0) {
+		infinite_reporting_loop(getppid(), getpid());
+	}
+	else {
+		pid2 = fork();
+		if (pid2 == -1){
+			printf("Error while creating second fork.\n");
+			exit(1);
+		}
+		else if (pid2 == 0){
+			infinite_reporting_loop(getppid(), getpid());
+		}
+		else{
+			system("ps -x");
+			infinite_reporting_loop(getppid(), getpid());
+		}
+	}
 	return 0;
 }
--- 3.c
+++ 3.c
@@ -1,28 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <errno.h> 
 
-int main(int argc, char* argv[], char* envp[])
-{
-	FILE* f;
-	char c;
+int power(int base, int p) {
+	if (p == 0)
+		return 1;
+	int result = 1;
+	while(p > 0) {
+		if (p % 2 == 1){
+			result *= base;
+		}
+		base *= base;
+		p /= 2;
+	}
+	return result;
+}
 
-	if(argc == 1)
+void exit_proccess(int signal) {
+	exit(0);
+}
+
+void proccess_infinite_reporting(int task_num, int currentPid, int parentPid) {
+//printf("Process %d with parent %d has number %d\n", currentPid, parentPid, task_num);
+	struct timeval time;
+	long sleepTimeMS = task_num * 200;
+	long long timestamp;
+	while(1) {
+		usleep(sleepTimeMS * 1000);
+		gettimeofday(&time, NULL);
+		timestamp = (((long long)time.tv_sec)*1000) + (time.tv_usec/1000);
+		printf("Node %d, PID %d, PARENT PID %d, timestamp %lld\n", task_num, currentPid, parentPid, timestamp);
+	}
+}
+
+void recursive_fork(int* totalProccessesStarted,  int leafsCount, int depthLeft) {
+	if (depthLeft == 0)
+		return;
+	pid_t pid;
+	for (int i = 0; i < leafsCount; i++) {
+		pid = fork();
+		if (pid == 0) {
+			int proccessNum = *totalProccessesStarted + 1;
+			int newCounter = proccessNum;
+ 			recursive_fork(&newCounter, leafsCount, depthLeft - 1);
+			proccess_infinite_reporting(proccessNum, getpid(), getppid());
+		}
+		else if (pid > 0){
+			if (leafsCount == 1)
+				*totalProccessesStarted += depthLeft;
+			else
+				*totalProccessesStarted += (power(leafsCount, depthLeft) - 1) / (leafsCount - 1);
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	signal(SIGINT, exit_proccess);
+	if (argc != 3)
 	{
-		printf("Enter output file as first argument. Make sure directory exists.\n");
+		printf("Pass amount of leafs in node as first argument, and tree height as second.\n");
 		exit(1);
 	}
 
-	f = fopen(argv[1], "w");
-	if (f == NULL) {
-		printf("Error while creating file %s.\n", argv[1]);
+	int totalProccessesStarted = 0;
+	int targetHeight = atoi(argv[2]);
+	int eachNodeLeafsCount = atoi(argv[1]);
+
+	if (targetHeight <= 0){
+		printf("Invalid target proccesses tree height.\n");
+		exit(1);
+	}
+	if (eachNodeLeafsCount < 0){
+		printf("Invalid target node leafs count.\n");
 		exit(1);
 	}
 
-	printf("Enter text. Press Ctrl+F to mark end of the file.\n");
-
-	while ((c = getc(stdin)) != 6)
-		putc(c,f);
-	fclose(f);
-
+	recursive_fork(&totalProccessesStarted, eachNodeLeafsCount, targetHeight);
+	wait(NULL);
 	return 0;
 }
